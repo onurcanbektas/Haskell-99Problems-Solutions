325d9bb88c3f5341142ea17e3c337a719d497c4e
onurcanbektas <onurcan.bektas@metu.edu.tr>
2018-03-10 17:25:19 +0200

    Update

--- Problem6.hs
+++ Problem6.hs
@@ -1,3 +1,29 @@
 module Q6 where
 
+-- @onurcanbektas
+
 import Problem5
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome xs
+ | ( (myReverse' xs) == xs ) = True
+ | otherwise = False
+
+
+-- Or, @onurcanbektas
+
+isPalindrome' :: Eq a => [a] -> Bool
+isPalindrome' []  = True
+isPalindrome' [_] = True
+isPalindrome' xs
+ |( head xs ) == (last xs) = isPalindrome' . tail . init $ xs
+ | otherwise = False
+
+-- Or
+
+isPalindrome'' :: (Eq a) => [a] -> Bool
+isPalindrome'' []  = True
+isPalindrome'' [_] = True
+isPalindrome'' xs  = (head xs) == (last xs) && (isPalindrome' $ init $ tail xs)
